e7af56599b782ab1ed7451134f223686741fd03a
KuRika28 <erika.de-jesus-pinto-pereira@etu.univ-orleans.fr>
2025-01-13 14:55:56 +0100

    5

--- SAE_serpiuto/source/serpent.py
+++ SAE_serpiuto/source/serpent.py
@@ -1,147 +1,112 @@
 # coding: utf-8
 """
-           SAE1.02 SERPIUT'O
-        BUT1 Informatique 2024-2025
+            SAE1.02 SERPIUT'O
+         BUT1 Informatique 2024-2025
 
-
-   Module serpent.py
-   Ce module implémente l'API permettant de gérer les informations des joueurs (idenfier à leur serpent)
+    Module serpent.py
+    Ce module implémente l'API permettant de gérer les informations des joueurs (idenfier à leur serpent)
 """
 import arene
 
-
 def Serpent(nom_joueur:str, num_joueur:int,points:int=0,positions:list=None,tps_s:int=0,tps_p:int=0,tps_m:int=0,direction:str='N')->dict:
-   """Créer un joueur avec toutes les informations le concernant.
+    """Créer un joueur avec toutes les informations le concernant.
 
+    Args:
+        nom_joueur (str): nom du joueur
+        num_joueur (int): numero du joueur
+        points (int, optional): nombre de points attribués au joueur. Defaults to 0.
+        positions (list, optional): la liste des positions occupées par le serpent sur l'arène. Defaults to None.
+        tps_s (int, optional): temps restant pour le bonus surpuissance. Defaults to 0.
+        tps_p (int, optional): temps restant pour le bonus protection. Defaults to 0.
+        tps_m (int, optional): temps restant pour le bonus mange-mur. Defaults to 0.
+        direction (str, optional): dernière direction prise par le serpent. Defaults to 'N'.
 
-   Args:
-       nom_joueur (str): nom du joueur
-       num_joueur (int): numero du joueur
-       points (int, optional): nombre de points attribués au joueur. Defaults to 0.
-       positions (list, optional): la liste des positions occupées par le serpent sur l'arène. Defaults to None.
-       tps_s (int, optional): temps restant pour le bonus surpuissance. Defaults to 0.
-       tps_p (int, optional): temps restant pour le bonus protection. Defaults to 0.
-       tps_m (int, optional): temps restant pour le bonus mange-mur. Defaults to 0.
-       direction (str, optional): dernière direction prise par le serpent. Defaults to 'N'.
-
-
-   Returns:
-       dict: une dictionnaire contenant les informations du serpent
-   """   
-   return {"nom_joueur":nom_joueur,"num_joueur":num_joueur,"points":points,"positions":positions,"tps_s":tps_s,"tps_p":tps_p,"tps_m":tps_m,"direction":direction}
-
+    Returns:
+        dict: une dictionnaire contenant les informations du serpent
+    """    
+    return {"nom":nom_joueur,"numero":num_joueur,"pos":positions,"temps_surp":tps_s,"temps_protec":tps_p,"temps_mange":tps_m,"der_direction":direction}
 
 def get_nom(serpent:dict)->str:
-   """retourne le nom du joueur associé au serpent
+    """retourne le nom du joueur associé au serpent
 
+    Args:
+        serpent (dict): le serpent considéré
 
-   Args:
-       serpent (dict): le serpent considéré
+    Returns:
+        str: le nom du joueur associé à ce serpent
+    """    
+    ...
 
-
-   Returns:
-       str: le nom du joueur associé à ce serpent
-   """  
-   return serpent["nom_joueur"]
-
-
-
-def get_num_joueur(serpent:dict)->int: 
+def get_num_joueur(serpent:dict)->int: #Erika
     """retourne le numéro du joueur associé au serpent
 
+    Args:
+        serpent (dict): le serpent considéré
 
-   Args:
-       serpent (dict): le serpent considéré
-
-
-   Returns:
-       int: le numéro du joueur associé à ce serpent
-   """  
+    Returns:
+        int: le numéro du joueur associé à ce serpent
+    """   
     return serpent["numero"]
 
+def get_points(serpent:dict)->int: #Erika
+    """retourne le nombre de points du joueur associé au serpent
 
-def get_points(serpent:dict)->int:
-   """retourne le nombre de points du joueur associé au serpent
-
-
-   Args:
-       serpent (dict): le serpent considéré
+    Args:
+        serpent (dict): le serpent considéré
 
     Returns:
         int: le nombre de points du joueur associé à ce serpent
-    """
-   return serpent["points"]
-
+    """   
+    return serpent["points"]
 
 def get_liste_pos(serpent:dict)->list:
-   """retourne la liste des positions occupées par le serpent sur l'arène. La première position étant la tête du serpent
+    """retourne la liste des positions occupées par le serpent sur l'arène. La première position étant la tête du serpent
 
+    Args:
+        serpent (dict): le serpent considéré
 
-   Args:
-       serpent (dict): le serpent considéré
-
-
-   Returns:
-       list: la liste des positions occupées par le serpent
-   """   
-   return serpent["positions"]
-
+    Returns:
+        list: la liste des positions occupées par le serpent
+    """    
+    ...
 
 def get_queue(serpent:dict)->list[int,int]:
-   """retourne la position (lig,col) de la queue du serpent dans l'arène
+    """retourne la position (lig,col) de la queue du serpent dans l'arène
 
+    Args:
+        serpent (dict): le serpent considéré
 
-   Args:
-       serpent (dict): le serpent considéré
-
-
-   Returns:
-       [int,int]: la position lig,col du la queue du serpent
-   """   
-   return list(serpent[get_liste_pos(serpent)[-1]])
-
+    Returns:
+        [int,int]: la position lig,col du la queue du serpent
+    """    
+    return list(serpent[get_liste_pos(serpent)[-1]])
 
 def get_derniere_direction(serpent:dict)->str:
-   """retourne la dernière direction choisie par le joueur pour se déplacer
+    """retourne la dernière direction choisie par le joueur pour se déplacer
 
+    Args:
+        serpent (dict): le serpent considéré
 
-   Args:
-       serpent (dict): le serpent considéré
-
-
-   Returns:
-       str: un des caractère N S E O
-   """   
-   return serpent["direction"]
-
+    Returns:
+        str: un des caractère N S E O
+    """    
+    return serpent["der_direction"]
 
 def get_bonus(serpent:dict)->list:
-   """retourne une liste contenant les bonus obtenus par le joueur
-       c'est-à-dire ceux pour lesquels le temps restant est supérieur à 0
+    """retourne une liste contenant les bonus obtenus par le joueur
+        c'est-à-dire ceux pour lesquels le temps restant est supérieur à 0
 
+    Args:
+        serpent (dict): le serpent considéré
 
-   Args:
-       serpent (dict): le serpent considéré
-
-
-   Returns:
-       list: la liste des bonus du joueur
-   """   
-   res=[]
-   if serpent["tps_s"]>0:
-      res.append(serpent["tps_s"])
-   if serpent["tps_p"]>0:
-      res.append(serpent["tps_p"])
-   if serpent["tps_m"]>0:
-      res.append(serpent["tps_m"])
+    Returns:
+        list: la liste des bonus du joueur
+    """    
+    ...
 
 
 
-
-
-
-
-def ajouter_points(serpent:dict,nb_points:int): 
+def ajouter_points(serpent:dict,nb_points:int): #Erika
     """ajoute (ou enlève) des points à un serpent
 
     Args:
@@ -150,7 +115,7 @@ def ajouter_points(serpent:dict,nb_points:int):
     """    
     serpent["points"]+=nb_points
 
-def set_liste_pos(serpent:dict, tete:list): 
+def set_liste_pos(serpent:dict, tete:list): #Erika
     """initialise la liste des positionsd'un serpent
 
     Args:
@@ -159,158 +124,124 @@ def set_liste_pos(serpent:dict, tete:list):
     """    
     serpent["pos"]=tete
 
-
 def set_derniere_direction(serpent:dict, direction:str):
-   """Met à jout la dernière direction utilisée par le serpent (utile pour l'affichage)
+    """Met à jout la dernière direction utilisée par le serpent (utile pour l'affichage)
 
-
-   Args:
-       serpent (dict): le serpent considéré
-       direction (str): un des caractère N S E O
-   """   
-   serpent["direction"]=direction
-
+    Args:
+        serpent (dict): le serpent considéré
+        direction (str): un des caractère N S E O
+    """    
+    ...
 
 def to_str(serpent:dict)->str:
-   """produit une chaine de caractères contenant les informations principales d'un serpent sour la forme
-   Joueur 1 -> 143 s:0 m:4 p:0
-   où Joueur 1 est le nom du joueur, après la flèche se trouve le nombre de point
-   puis le temps restant de chaque bonus (supuissante, mange mur et protection)
+    """produit une chaine de caractères contenant les informations principales d'un serpent sour la forme
+    Joueur 1 -> 143 s:0 m:4 p:0
+    où Joueur 1 est le nom du joueur, après la flèche se trouve le nombre de point
+    puis le temps restant de chaque bonus (supuissante, mange mur et protection)
 
+    Args:
+        serpent (dict): le serpent considéré
 
-   Args:
-       serpent (dict): le serpent considéré
-
-
-   Returns:
-       str: la chaine de caractères donnant les informations principales d'un serpent
-   """   
-   return f"{get_nom(serpent)} -> {get_points(serpent)} s:{get_temps_surpuissance(serpent)} m:{get_temps_mange_mur(serpent)} p:{get_temps_protection(serpent)}"
-
-
+    Returns:
+        str: la chaine de caractères donnant les informations principales d'un serpent 
+    """    
+    ...
 
 def get_temps_protection(serpent:dict)->int:
-   """indique le temps restant pour le bonus protection
+    """indique le temps restant pour le bonus protection
 
-
-   Args:
-       serpent (dict): le serpent considéré
-
-
-   Returns:
-       int: le nombre de tours restant pour ce bonus
-   """   
-   return serpent["tps_p"]
-
-
-def get_temps_mange_mur(serpent:dict)->int:
-   """indique le temps restant pour le bonus mange mur
-
-
-   Args:
-       serpent (dict): le serpent considéré
-
-
-   Returns:
-       int: le nombre de tours restant pour ce bonus
-   """  
-   return serpent["tps_m"]
-
-
-
-def get_temps_surpuissance(serpent:dict)->int:
-   """indique le temps restant pour le bonus surpuissance
-
-
-   Args:
-       serpent (dict): le serpent considéré
-
-
-   Returns:
-       int: le nombre de tours restant pour ce bonus
-   """  
-   return serpent["temps_surp"]
-
-
-def ajouter_temps_protection(serpent:dict, temps:int)->int:
-   """ajoute du temps supplémentaire pour le bonus protection
-
+    Args:
+        serpent (dict): le serpent considéré
 
     Returns:
         int: le nombre de tours restant pour ce bonus
-    """
-   serpent["tps_p"]+=temps
-   return get_temps_protection(serpent)
+    """    
+    ...
 
+def get_temps_mange_mur(serpent:dict)->int:
+    """indique le temps restant pour le bonus mange mur
 
+    Args:
+        serpent (dict): le serpent considéré
+
+    Returns:
+        int: le nombre de tours restant pour ce bonus
+    """   
+    ...
+
+def get_temps_surpuissance(serpent:dict)->int: #Erika
+    """indique le temps restant pour le bonus surpuissance
+
+    Args:
+        serpent (dict): le serpent considéré
+
+    Returns:
+        int: le nombre de tours restant pour ce bonus
+    """   
+    return serpent["temps_surp"]
+
+def ajouter_temps_protection(serpent:dict, temps:int)->int: #Erika
+    """ajoute du temps supplémentaire pour le bonus protection
+
+    Args:
+        serpent (dict): le serpent considéré
+        temps (int): le nombre de tours à ajouter
+
+    Returns:
+        int: le nombre de tours total restant pour ce bonus
+    """    
+    return get_temps_protection(serpent)+temps
 
 def ajouter_temps_mange_mur(serpent:dict, temps:int)->int:
-   """ajoute du temps supplémentaire pour le bonus mange mur
+    """ajoute du temps supplémentaire pour le bonus mange mur
 
+    Args:
+        serpent (dict): le serpent considéré
+        temps (int): le nombre de tours à ajouter
 
-   Args:
-       serpent (dict): le serpent considéré
-       temps (int): le nombre de tours à ajouter
-
-
-   Returns:
-       int: le nombre de tours total restant pour ce bonus
-   """   
-   serpent["tps_m"]+=temps
-
+    Returns:
+        int: le nombre de tours total restant pour ce bonus
+    """    
+    ...
 
 def ajouter_temps_surpuissance(serpent:dict, temps:int)->int:
-   """ajoute du temps supplémentaire pour le bonus surpuissance
+    """ajoute du temps supplémentaire pour le bonus surpuissance
 
+    Args:
+        serpent (dict): le serpent considéré
+        temps (int): le nombre de tours à ajouter
 
-   Args:
-       serpent (dict): le serpent considéré
-       temps (int): le nombre de tours à ajouter
-
-
-   Returns:
-       int: le nombre de tours total restant pour ce bonus
-   """   
-   serpent["tps_s"]+=temps
-   return get_temps_surpuissance(serpent)
-
+    Returns:
+        int: le nombre de tours total restant pour ce bonus
+    """    
+    ...
 
 def maj_temps(serpent:dict):
-   """Décrémente les temps restant pour les bonus de ce serpent
-   Attention les temps ne peuvent pas être négatif
+    """Décrémente les temps restant pour les bonus de ce serpent
+    Attention les temps ne peuvent pas être négatif
 
-
-   Args:
-       serpent (dict): le serpent considéré
-   """
-   if serpent["tps_s"] >0:
-    serpent["tps_s"]-=1
-   if serpent["tps_p"] >0:
-    serpent["tps_p"]-=1
-   if serpent["tps_m"] >0:
-    serpent["tps_m"]-=1
+    Args:
+        serpent (dict): le serpent considéré
+    """    
+    ...
 
 def serpent_2_str(serpent:dict, sep=";")->str:
-   """Sérialise un serpent sous la forme d'une chaine de caractères
-   contenant 2 lignes.
-   nom_j;num_j;nb_point;tps_surpuissance;tps_mange_mur;tps_protection
-   lig1;col1;lig2;col2;...
-   La première ligne donne les informations autres que la liste des positions du serpent
-   la deuxième ligne donné la liste des position du serpent en commençant par la tête
-   Args:
-       serpent (dict): le serpent considéré
-       sep (str, optional): le caractère séparant les informations du serpent. Defaults to ";".
+    """Sérialise un serpent sous la forme d'une chaine de caractères
+    contenant 2 lignes.
+    nom_j;num_j;nb_point;tps_surpuissance;tps_mange_mur;tps_protection
+    lig1;col1;lig2;col2;...
+    La première ligne donne les informations autres que la liste des positions du serpent
+    la deuxième ligne donné la liste des position du serpent en commençant par la tête
+    Args:
+        serpent (dict): le serpent considéré
+        sep (str, optional): le caractère séparant les informations du serpent. Defaults to ";".
 
+    Returns:
+        str: la chaine de caractères contenant les toutes informations du serpent
+    """    
+    ...
 
-   Returns:
-       str: la chaine de caractères contenant les toutes informations du serpent
-   """   
-   pos=[str(x) for x in serpent["positions"]]
-   return f"{serpent['nom_joueur']}{sep}{serpent['num_joueur']}{sep}{serpent['points']}{sep}{serpent['tps_s']}{sep}{serpent['tps_p']}{sep}{serpent['tps_m']} \n"+' '.join(pos)
-
-
-
-def serpent_from_str(la_chaine, sep=";")->dict: 
+def serpent_from_str(la_chaine, sep=";")->dict: #Erika
     """Reconstruit un serpent à partir d'une chaine de caractères
        telle que celle produite par la fonction précédente
 
@@ -322,9 +253,11 @@ def serpent_from_str(la_chaine, sep=";")->dict:
         dict: Le serpent représenté dans la chaine de caractères
     """    
     nom_joueur,num_joueur,nb_points,positions,tps_s,tps_p,tps_m,direction=la_chaine.split(sep)
-    return {"nom":nom_joueur, "numero":num_joueur, "points":nb_points, "pos":positions,"temps_surp":tps_s,"temps_protec":tps_p,"temps_mange":tps_m,"der_direction":direction}
+    positions=eval(positions)
+    return {"nom":nom_joueur, "numero":int(num_joueur), "points":int(nb_points), "pos":positions,"temps_surp":int(tps_s),
+            "temps_protec":int(tps_p),"temps_mange":int(tps_m),"der_direction":direction}
 
-def copy_serpent(serpent:dict)->dict: 
+def copy_serpent(serpent:dict)->dict: #Erika
     """fait une copie du serpent passer en paramètres
     Attention à bien faire une copie de la liste des positions
         
@@ -334,8 +267,6 @@ def copy_serpent(serpent:dict)->dict:
     Returns:
         dict: la copie du serpent passé en paramètres
     """ 
-
-    serpent2={"nom":get_nom(serpent),"numero":get_num_joueur(serpent),"points":get_points(serpent),"positions":get_liste_pos,"temps_surp":get_temps_surpuissance(serpent),"temps_protec":get_temps_protection(serpent),"temps":(serpent),"temps_mange":get_temps_mange_mur(serpent),"der_direction":get_derniere_direction(serpent)}
-    return serpent2
-
-
+    return {"nom":get_nom(serpent),"numero":get_num_joueur(serpent),"points":get_points(serpent),"positions":get_liste_pos,"temps_surp":get_temps_surpuissance(serpent),"temps_protec":get_temps_protection(serpent),"temps":(serpent),"temps_mange":get_temps_mange_mur(serpent),"der_direction":get_derniere_direction(serpent)}
+    
+print(serpent_from_str("Erika,2,28,[(1,1),(1,2),(1,3)],0,1,0,N"))
