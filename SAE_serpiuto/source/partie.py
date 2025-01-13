549797adec9846eb543e258fc69cd13cece1a07b
KuRika28 <erika.de-jesus-pinto-pereira@etu.univ-orleans.fr>
2025-01-13 11:23:22 +0100

    4

--- SAE_serpiuto/source/partie.py
+++ SAE_serpiuto/source/partie.py
@@ -219,4 +219,6 @@ def copy_partie(partie:dict)->dict:
     Returns:
         dict: la recopie de la partie passée en paramètres
     """
-    ...
+    partie2={"nom_partie":get_nom_partie(partie), "duree_totale":get_duree_totale(partie),"temps_restant":get_temps_restant(partie),
+            "arene":get_arene(partie)}
+    return partie2
