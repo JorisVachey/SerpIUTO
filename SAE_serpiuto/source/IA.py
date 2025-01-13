0ba61a1f59e15fbff823d38903045f7a3d5dc4a6
Joris Vachey <joris.vachey@etu.univ-orleans.fr>
2025-01-13 16:54:57 +0100

    fix: implement direction logic in directions_possibles function for serpent movement

--- SAE_serpiuto/source/IA.py
+++ SAE_serpiuto/source/IA.py
@@ -36,8 +36,19 @@ def directions_possibles(l_arene:dict,num_joueur:int)->str:
             qu'aucune direction ne soit possible donc la fonction peut retourner la chaine vide
     """    
     res=""
-    serp=arene.get_serpent[l_arene,num_joueur]
-    pass
+    serp=[arene.get_serpent[l_arene,num_joueur][0],arene.get_serpent[l_arene,num_joueur][1]]
+    val_tete=arene.get_val_boite(l_arene,serp[0],serp[1])
+    mat=arene["matrice"]
+    lgn,col=arene.get_dim(arene)
+    if 0<=serp[0]-1 <= lgn and arene.est_mur(serp[1],serp[0]-1)!=True and arene.get_val_boite(l_arene,serp[0]-1,serp[1])<=val_tete:
+        res+="N"
+    if col>=serp[1]+1 >= 0 and arene.est_mur(serp[0],serp[1]+1)!=True and arene.get_val_boite(l_arene,serp[0],serp[1]+1)<=val_tete: 
+        res+="O"
+    if 0<=serp[0]+1 <= lgn and arene.est_mur(serp[1],serp[0]+1)!=True and arene.get_val_boite(l_arene,serp[0]+1,serp[1])<=val_tete:
+        res+="S"
+    if col>=serp[1]-1 >= 0 and arene.est_mur(serp[0],serp[1]-1)!=True and arene.get_val_boite(l_arene,serp[0],serp[1]-1)<=val_tete:
+        res+="E"
+
 
 def objets_voisinage(l_arene:dict, num_joueur, dist_max:int):
     """Retourne un dictionnaire indiquant pour chaque direction possibles, 
