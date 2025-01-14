dc7c026348615b7998af1dd15160a24918dc115b
KuRika28 <erika.de-jesus-pinto-pereira@etu.univ-orleans.fr>
2025-01-14 14:15:06 +0100

    1415

--- SAE_serpiuto/source/IA.py
+++ SAE_serpiuto/source/IA.py
@@ -64,55 +64,19 @@ def directions_possibles(l_arene:dict,num_joueur:int)->str:
     if col>=serp[1]-1 >= 0 and not arene.est_mur(serp[0],serp[1]-1) and arene.get_val_boite(l_arene,serp[0],serp[1]-1)<=val_tete:
         res+="E"
 
-def objets_voisinage(l_arene:dict, num_joueur, dist_max:int):
-    """Retourne un dictionnaire indiquant pour chaque direction possibles, 
-        les objets ou boites pouvant être mangés par le serpent du joueur et
-        se trouvant dans voisinage de la tête du serpent 
-
-    Args:
-        l_arene (dict): l'arène considérée
-        num_joueur (int): le numéro du joueur considéré
-        dist_max (int): le nombre de cases maximum qu'on s'autorise à partir du point de départ
-    Returns:
-        dict: un dictionnaire dont les clés sont des directions  et les valeurs une liste de triplets
-            (distance,val_objet,prop) où distance indique le nombre de cases jusqu'à l'objet et id_objet
-            val_obj indique la valeur de l'objet ou de la boite et prop indique le propriétaire de la boite
-    """
-    res = {"N": [], "S": [], "E": [], "O": []}
-    serpent = arene.get_serpent(l_arene, num_joueur)[0]
-    tete = serpent["pos"][0]
-    calque = calque(l_arene, num_joueur)
-
-    directions = {
-        "N": (-1, 0),
-        "S": (1, 0),
-        "E": (0, 1),
-        "O": (0, -1)
-    }
-
-    for direction,(dx, dy) in directions.items():
-        for dist in range(1,dist_max+1):
-            pos=(tete[0]+dx*dist,tete[1]+dy*dist)
-            if est_sur_arene(l_arene,pos):
-                val=get(calque,pos)
-                if val is not None:
-                    obj=arene.get_objet(l_arene, pos)
-                    if obj:
-                        res[direction].append((dist, obj["valeur"], obj["proprietaire"]))
-                    elif arene.est_boite(l_arene, pos):
-                        boite = arene.get_boite(l_arene, pos)
-                        res[direction].append((dist, boite["valeur"], boite["proprietaire"]))
-
-    return res
-    
-
-    return res
-
 def get(l_arene,pos):
     if est_sur_arene(l_arene,pos):
         return matrice.get_val(l_arene["matrice"], pos[0], pos[1])
 
 def calque(l_arene,num_joueur:int):
+    """Fabrique le calcque de l'arene en utilisant l'inondation
+    Args:
+        l_arene (dict): l'arène considérée
+        num_joueur (int): le numéro du joueur considéré
+
+    Returns:
+        matrice: le calcque de l'arène créé à partir de l'inondation
+    """
     lgn,col=arene.get_dim(l_arene)
     calque=matrice.Matrice(lgn,col)
     i=0
@@ -131,7 +95,38 @@ def calque(l_arene,num_joueur:int):
         i+=1
     return calque
 
+def objets_voisinage(l_arene:dict, num_joueur, dist_max:int):
+    """Retourne un dictionnaire indiquant pour chaque direction possibles, 
+        les objets ou boites pouvant être mangés par le serpent du joueur et
+        se trouvant dans voisinage de la tête du serpent 
 
+    Args:
+        l_arene (dict): l'arène considérée
+        num_joueur (int): le numéro du joueur considéré
+        dist_max (int): le nombre de cases maximum qu'on s'autorise à partir du point de départ
+    Returns:
+        dict: un dictionnaire dont les clés sont des directions  et les valeurs une liste de triplets
+            (distance,val_objet,prop) où distance indique le nombre de cases jusqu'à l'objet et id_objet
+            val_obj indique la valeur de l'objet ou de la boite et prop indique le propriétaire de la boite
+    """
+    res={"N":[],"S":[],"E":[],"O":[]}
+    serp=arene.get_serpent(l_arene, num_joueur)[0]
+    tete=serp["pos"][0]
+    calque=calque(l_arene, num_joueur)
+    directions={"N":(-1, 0),"S":(1, 0),"E":(0, -1),"O":(0, +1)}
+    for direction,(dx, dy) in directions.items():
+        for dist in range(1,dist_max+1):
+            pos=(tete[0]+dx*dist,tete[1]+dy*dist)
+            if est_sur_arene(l_arene,pos):
+                val=get(calque,pos)
+                if val is not None:
+                    obj=arene.get_objet(l_arene, pos)
+                    if obj:
+                        res[direction].append((dist,obj["valeur"],obj["proprietaire"]))
+                    elif arene.est_boite(l_arene,pos):
+                        boite=arene.get_boite(l_arene,pos)
+                        res[direction].append((dist,boite["valeur"],boite["proprietaire"]))
+    return res
 
 def mon_IA2(num_joueur:int, la_partie:dict)->str:
     return 'N'
